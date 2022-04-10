b3d25f59e700db91a3024ec25ee2187b2e6553d5
Brennast26 <brennast@mail.uc.edu>
2022-04-10 19:15:12 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	    for(int cnt=0; cnt <=30; cnt++)
+        {
+            System.out.println(cnt);
+        }
+    }
+}
